26c307202d3519241e5df1456635f059057b1a42
Ian-Freitag <ian.freitag16@gmail.com>
2022-05-10 10:50:43 -0300

    Banco Criado
    banco de dados criado e requisicao passa dos 30 porem, tem um timer

--- db.py
+++ db.py
@@ -1,3 +1,28 @@
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import Session
+import sqlite3
+conn = sqlite3.connect('banco.db')
 
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS tb_veiculo(
+    idVeiculo INTEGER PRIMARY KEY AUTOINCREMENT,
+    marca TEXT NOT NULL,
+    modelo TEXT NOT NULL,
+    versao_veic TEXT NOT NULL,
+    valor_veic FLOAT NOT NULL,
+    ano_veic INT NOT NULL,
+    ano_frab INT NOT NULL,
+    carroceria TEXT NOT NULL,
+    km_veic FLOAT NOT NULL,
+    cor TEXT NOT NULL,
+    vendedor TEXT NOT NULL,
+    cidade TEXT NOT NULL,
+    estado TEXT NOT NULL
+)
+""")
+
+conn.commit()
+
+print("tabela criada com sucesso.")
+
+conn.close()
--- requisicoes.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get('https://www.kavak.com/br/ordem-menor-preco/carros-usados')
-
-print(response.status_code)
