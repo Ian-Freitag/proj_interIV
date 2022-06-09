a6a55545f9721d2ad098efb2be472c81cd7103a4
pedropaselar <pedropaselar@gmail.com>
2022-06-09 12:13:24 -0300

    amem foiiiii

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express')
 const { use } = require('express/lib/application')
 const app = express()
 const port = 3000
+const path = require('path')
 
 app.set('view engine', 'ejs')
 app.use(express.json());
-app.use(express.static(__dirname + '/public/s'));
+app.use('/',express.static(path.join(__dirname + '/css')));
 
 app.get('/', (req, res) => {
     res.render('index')
